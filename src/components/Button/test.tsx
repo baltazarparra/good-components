af9ee4ccb9f9fce542c578730d8d81c94fd0e046
baltazarparra <baltazar.parra.ext@dasa.com.br>
2021-04-07 09:09:13 -0300

    Set full width version of button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,16 +1,19 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helper'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
   it('should render medium button', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
   it('should render small button', () => {
     renderWithTheme(<Button size="small">Buy now</Button>)
@@ -26,5 +29,18 @@ describe('<Button />', () => {
       padding: '0.8rem 4.8rem',
       'font-size': '1.6rem'
     })
+  })
+  it('should render full width button', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+  it('should render button with icon', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument
+    expect(screen.getByTestId('icon')).toBeInTheDocument
   })
 })
